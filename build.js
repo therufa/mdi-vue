fffaaab97912d919b005375483ef5f2931e7d16c
Attila Max Ruf <attila.ruf@sclable.com>
2017-05-18 18:54:39 +0200

    feat(): package.json is now created for the npm package

--- build.js
+++ build.js
@@ -2,14 +2,31 @@ const path  = require('path')
 const fs    = require('fs')
 
 const buildPath   = path.resolve(__dirname, 'build')
+const distPath    = path.resolve(__dirname, 'dist')
 const svgPath     = path.resolve(__dirname, 'mdi/icons/svg')
 
-const toPascalCase = (str) => {
+const tplFile     = path.resolve(__dirname, 'build.tpl')
+const pkgFile     = path.resolve(__dirname, 'package.json')
+
+function toPascalCase(str) {
   return str.toLowerCase()
     .split(/-/g).map(token => 
       token.charAt(0).toUpperCase() + token.slice(1))
     .join('')
 }
+
+const copyPackage = () => fs.readFile(pkgFile, (err, pkgRaw) => {
+  const pkgObj = JSON.parse(pkgRaw)
+
+  // Nobody cares for errors... nobody
+  fs.writeFile(path.resolve(distPath, 'package.json'), JSON.stringify(Object.keys(pkgObj)
+    .map((key) => {
+      if (!/script|devDependencies|dependencies/.test(key)) {
+        return pkgObj[key]
+      }
+    })
+  ))
+})
 
 const buildIcons = (components) => fs.readFile(path.resolve('./build.tpl'), (err, tpl) => {
   for (const component of components) {
@@ -20,6 +37,8 @@ const buildIcons = (components) => fs.readFile(path.resolve('./build.tpl'), (err
         .replace(/\{\{path\}\}/g, component.path)
     )
   }
+
+  copyPackage()
 })
 
 fs.readdir(svgPath, (a, svgs) => {
