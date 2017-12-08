2949c28d30c08b88da365a374ea6b780ee1201da
Attila Max Ruf <attila.ruf@sclable.com>
2017-12-08 21:08:24 +0100

    refactor: clean up code

--- build.js
+++ build.js
@@ -1,62 +1,88 @@
-const path      = require('path')
-const fs        = require('fs')
+const path = require('path')
+const fs = require('fs')
+const jsonFormat = require('json-format')
+const { promisify } = require('util')
 const kebabCase = require('lodash').kebabCase
 
-const buildPath   = path.resolve(__dirname, 'build')
-const distPath    = path.resolve(__dirname, 'dist')
-const svgPath     = path.resolve(__dirname, 'mdi/icons/svg')
+const readFileAsync = promisify(fs.readFile)
+const writeFileAsync = promisify(fs.writeFile)
 
-const tplFile     = path.resolve(__dirname, 'build.tpl')
-const pkgFile     = path.resolve(__dirname, 'package.json')
+const BUILD_PATH = path.resolve(__dirname, 'build')
+const DIST_PATH = path.resolve(__dirname, 'dist')
+const SVG_PATH = path.resolve(__dirname, 'mdi/icons/svg')
+const TPL_PATH = path.resolve(__dirname, 'build.tpl')
+const PKG_FILE = path.resolve(__dirname, 'package.json')
 
-function toPascalCase(str) {
-  return str.toLowerCase()
-    .split(/-/g).map(token => 
-      token.charAt(0).toUpperCase() + token.slice(1))
+function toPascalCase (str) {
+  return str
+    .toLowerCase()
+    .split(/-/g)
+    .map(token => token.charAt(0).toUpperCase() + token.slice(1))
     .join('')
 }
 
-const copyPackage = () => fs.readFile(pkgFile, (err, pkgRaw) => {
-  const pkgObj = JSON.parse(pkgRaw)
-
-  // Nobody cares for errors... nobody
-  Object.keys(pkgObj).map((key) => {
-    if (/devDependencies|dependencies/.test(key)) {
-      delete pkgObj[key]
+const clearPackage = packageData => {
+  return Object.keys(packageData).reduce((p, key) => {
+    if (!/devDependencies|dependencies/.test(key)) {
+      return { ...p, [key]: packageData[key] }
     }
+
+    return p
+  }, {})
+}
+
+const copyPackage = async (sourcePath, targetPath) => {
+  const packageData = await readFileAsync(sourcePath, { encoding: 'utf8' })
+  const cleanPackageData = clearPackage(JSON.parse(packageData))
+
+  writeFileAsync(targetPath, jsonFormat(cleanPackageData, {
+    type: 'space',
+    size: 2
+  }))
+}
+
+const populateTemplate = (template, component) => {
+  return {
+    name: component.name,
+    content: template
+      .replace(/\{\{icon\}\}/g, kebabCase(component.name))
+      .replace(/\{\{path\}\}/g, component.path)
+  }
+}
+
+const buildIconComponents = async (templatePath, components) => {
+  const template = await readFileAsync(templatePath)
+
+  return components.map(component =>
+    populateTemplate(template.toString('utf-8'), component)
+  )
+}
+
+const writeIconComponents = async (buildPath, components) => {
+  components.forEach(component => {
+    writeFileAsync(path.join(buildPath, `${component.name}Icon.js`), component.content)
   })
+}
 
-  fs.writeFileSync(path.resolve(distPath, 'package.json'), JSON.stringify(pkgObj))
-})
+const buildIconBodyList = (svgPath, svgList) => {
+  return svgList.map(svg => ({
+    name: toPascalCase(svg).slice(0, -4),
+    path: async () => {
+      const svgFile = await readFileAsync(path.join(svgPath, svg))
+      const matches = /\sd="(.*)"/.exec(svgFile)
 
-const buildIcons = (components) => fs.readFile(path.resolve('./build.tpl'), (err, tpl) => {
-  for (const component of components) {
-    fs.writeFileSync(
-      path.join(buildPath,`${component.name}Icon.js`),
-      tpl.toString('utf-8')
-        .replace(/\{\{icon\}\}/g, kebabCase(component.name))
-        .replace(/\{\{path\}\}/g, component.path)
-    )
+      return matches ? matches[0] : undefined
+    }
+  }))
+}
+
+fs.readdir(SVG_PATH, async (err, svgList) => {
+  if (err) {
+    throw new Error(err)
   }
 
-  copyPackage()
+  const svgBodyList = buildIconBodyList(SVG_PATH, svgList)
+  const iconComponents = await buildIconComponents(TPL_PATH, svgBodyList)
+  await writeIconComponents(BUILD_PATH, iconComponents)
+  await copyPackage(PKG_FILE, path.resolve(DIST_PATH, 'package.json'))
 })
-
-fs.readdir(svgPath, (a, svgs) => {
-  buildIcons(svgs.map(svg => {
-    return { 
-      name: toPascalCase(svg).slice(0, -4),
-      path: (() => {
-          const matches = /\sd="(.*)"/.exec(fs.readFileSync(path.join(svgPath, svg), {
-            encoding: 'utf8'
-          }))
-
-          if (matches) {
-            return matches[0]
-          }
-      })()
-    }
-  }).filter(_ => _.path))
-})
-
-
